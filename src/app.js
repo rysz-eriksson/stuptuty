bc0a8272930e6856c71323fa5f700615ed453f5a
rysz-eriksson <ryszard.wolosiuk@gmail.com>
2020-03-31 13:05:45 +0200

    refactor: split js files, add images and views fun

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 import { uuid } from 'uuidv4';
+import './products'
+import './views'
+import './images';
+
+import { addedProductVisible } from './views'
 
 // array for storing chosen products
 let selectedProducts = []
@@ -10,52 +15,6 @@ const loadProducts = () => {
 }
 
 selectedProducts = loadProducts()
-
-let priceTag = document.querySelector('.price > h2')
-
-// changing the value of quantity
-document.querySelectorAll('.change-quantity').forEach((item) => {
-    item.addEventListener('click', (e) => {
-        const quantityField = document.querySelector('[name=quantity]')
-        // let x = quantityField.value
-        if (e.target.value === '+') {
-            quantityField.value = parseInt(quantityField.value) + 1
-        } else {
-            if (quantityField.value > 1) {
-                quantityField.value = quantityField.value -1
-            }
-        }
-        priceTag.textContent = `${150 * quantityField.value} zł`
-    })
-})
-
-// update the price value if changed by the input type number UI
-
-if (window.location.href.includes('product.html')) {
-    document.querySelector('[name=quantity]').addEventListener('change', (e) => {
-        const quantity = e.target.value
-        priceTag.textContent = `${150 * quantity} zł`
-
-    })
-}
-
-
-// event handler for manipulating labels on contact_form
-
-const formInputs = document.querySelectorAll('.form__input')
-
-formInputs.forEach((item) => {
-    item.addEventListener('focus', (e) => {
-    const labelEl = e.target.labels[0]
-    labelEl.classList.add('label__active')
-    })
-    item.addEventListener('blur', (e) => {
-        if (item.value.length === 0) {
-            const labelEl = e.target.labels[0]
-            labelEl.classList.remove('label__active')
-        }
-    })
-})
 
 // adding product to the basket
 const addProduct = (e) => {
@@ -94,7 +53,7 @@ const removeProductItem = (id) => {
     }
 }
 
-// basket icon logic
+// basket view logic
 
 const renderBasketView = () => {
     const basketContent = document.querySelector('.basket-content > p:nth-child(1)')
@@ -159,13 +118,7 @@ const renderBasketView = () => {
 
 renderBasketView()
 
-const addedProductVisible = () => {
-    const elem = document.querySelector('.added-to-basket')
-    elem.classList.add('visible')
-    elem.addEventListener('animationend', () => {
-        elem.classList.remove('visible')
-    })
-}
+
 
 // event handler for submitting new product to the basket
 document.querySelector('#product-form').addEventListener('submit', (e) => {
@@ -180,74 +133,10 @@ document.querySelector('#product-form').addEventListener('submit', (e) => {
 // event  handler for change in the local storage coming from a different page
 window.addEventListener('storage', (e) => {
     if (e.key === 'selectedProducts') {
-        console.log('bum')
         loadProducts()
         renderBasketView()
     }
 })
 
 
-// images slider logic
 
-// creating an array with thumbnails objects
-
-let images = []
-for (let i=1; i < 6; i++) {
-    images.push({
-        index: i,
-        src: `images/stuptuty-slider${i}.jpg`
-    })
-}
-
-// load slider__item on clicking the thumbnail
-const loadBigImage = (e) => {
-    console.log(e.target.src)
-    const chosenImage = images.find((item) => e.target.src.includes(item.src))
-    const bigImage = document.querySelector('.slider__item > img')
-    bigImage.src = chosenImage.src
-    bigImage.setAttribute('data-key', `${chosenImage.index}`)
-}
-
-// populate slider__thumbNav with images from an array
-
-images.forEach((image) => {
-    const imgEl = document.createElement('img')
-    imgEl.src = image.src
-    imgEl.classList.add('slider__thumbItem')
-    document.querySelector('.slider__thumbNav').appendChild(imgEl)
-    imgEl.addEventListener('click', loadBigImage)
-})
-document.querySelector('.slider__item').addEventListener('click', () => {
-    console.log(event)
-})
-
-// move slider__thumbNav on clicking an arrow
-
-document.querySelector('.slider__prev').addEventListener('click', () => {
-    const sliderDiv = document.querySelector('.slider__thumbNav')
-    let offsetValue = sliderDiv.style.transform.length > 0 ? sliderDiv.style.transform.slice(10, -3) : 0
-    let offset = offsetValue
-    if (offset != 0) {
-    const move = sliderDiv.animate([
-        {transform: `translate(${parseInt(offset)}px)`},
-        {transform: `translate(${parseInt(offset) + 205}px)`}
-    ], 500)
-    move.addEventListener('finish', () => {
-        sliderDiv.style.transform = `translate(${parseInt(offset) + 205}px)`;
-        })
-    }
-})
-document.querySelector('.slider__next').addEventListener('click', () => {
-    const sliderDiv = document.querySelector('.slider__thumbNav')
-    let offsetValue = sliderDiv.style.transform.length > 0 ? sliderDiv.style.transform.slice(10, -3) : 0
-    let offset = offsetValue
-    if (Math.abs(offset) < (images.length - 3) * 205) {
-    const move = sliderDiv.animate([
-        {transform: `translate(${offset}px)`},
-        {transform: `translate(${offset - 205}px)`}
-    ], 500)
-    move.addEventListener('finish', () => {
-        sliderDiv.style.transform = `translate(${offset - 205}px)`;
-        })
-    }
-})
